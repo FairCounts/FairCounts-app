2948352836074bbff8c46e74faf26ab3d156a6c4
Thomas Keunebroek <tkeunebr@tkeunebr-laptop.local>
2013-09-16 11:53:08 -0400

    Add a very basic Angular expenses app management

---
+++ js/controller.js
@@ -0,0 +1,22 @@
+function FairCountsController($scope) {
+  $scope.expenses = [
+    {
+      name: "New York",
+      amount: 10,
+      currency: "$",
+      paid: false
+    },
+    {
+      name: "Drinks at URI",
+      amount: 50,
+      currency: "$",
+      paid: false
+    },
+    {
+      name: "Boston housing",
+      amount: 50,
+      currency: "$",
+      paid: false
+    }
+  ];
+}
