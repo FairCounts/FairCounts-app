79422ef6d6403a017dbd472992e402a722f216a7
Théotime <theotime.loiseau@gmail.com>
2013-11-22 20:37:19 -0500

    Moved files to clean the file structure + changed a to button to follow better practices + better AngularJS

--- src/FairCounts/MainBundle/Resources/public/js/app.js
+++ src/FairCounts/MainBundle/Resources/public/js/app.js
@@ -1,18 +1,8 @@
-function FairCountsController($scope, $location) {
-  $scope.participants = [{name: "John Doe With A Very Very Very Very Very Long Name Seriously"}, 
-                        {name: "John Doe"},
-                        {name: "John Doe"}];
-  
-  $scope.addParticipant = function() {
-    $scope.participants.push({name:$scope.participantName});
-    $scope.participantName = '';
-    $location.path('/newGroup');
-  };
+var fairCounts = angular.module('FairCounts', []);
 
-  $scope.removeParticipant = function(index) {
-    $scope.participants.splice( $scope.participants.indexOf(index), 1 );
-  };
-  
+var controllers = {};
+
+controllers.mainController = function ($scope) {
   $scope.expenses = [
     {
       name: "New York",
@@ -33,4 +23,26 @@ function FairCountsController($scope, $location) {
       paid: false
     }
   ];
-}
+};
+
+controllers.newGroupController = function ($scope) {
+  $scope.participants = [{name: "John Doe With A Very Very Very Very Very Long Name Seriously"}, 
+                        {name: "John Doe"},
+                        {name: "John Doe"}];
+  
+  $scope.addParticipant = function() {
+    if ($scope.participantName && $scope.participantName !== "") {
+      $scope.participants.push({name:$scope.participantName});
+      $scope.participantName = '';
+    }
+  };
+
+  $scope.removeParticipant = function(index) {
+    $scope.participants.splice( $scope.participants.indexOf(index), 1 );
+  };
+};
+
+controllers.addExpenseController = function ($scope) {
+};
+
+fairCounts.controller(controllers);
